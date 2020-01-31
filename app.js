532dfd48c4ea05c6b749aef10422cc061f8d2f51
James Baptiste <Jamesbaptiste@lewisu.edu>
2020-01-31 00:48:50 -0600

    change code

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-var http = require("http");
+// var http = require("http");
 
+// http
+//   .createServer(function(req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.end("Hello World!");
+//   })
+//   .listen(8080);
+var http = require("http");
+var port = process.env.port || 1337;
 http
   .createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World\n");
   })
-  .listen(8080);
+  .listen(port);
